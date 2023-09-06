1c98b3b2661a3e10fdcb1ac3487bff24e91009b7
terry <munu220@gmail.com>
2023-09-06 17:04:40 +0000

    [feat] User 파트완료

--- follow_app/tweet/serializer.py
+++ follow_app/tweet/serializer.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
 from .models import Follow, Image, Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        read_only_fields = ["owner", "created_at", "updated_at"]
--- follow_app/tweet/views.py
+++ follow_app/tweet/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework import status
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Follow, Image, Post
+from .serializer import PostSerializer
 
 
 class UserViewSet(viewsets.ViewSet):
@@ -20,5 +22,9 @@ class UserViewSet(viewsets.ViewSet):
             )
         return Response(status=status.HTTP_200_OK, data=data)
 
+    @action(detail=False, methods=["get"])
     def posts(self, request, *args, **kwargs):
-        return Response({"message": "Hello, world!"})
+        user = request.user
+        posts = Post.objects.filter(owner=user)
+        serializer = PostSerializer(posts, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
