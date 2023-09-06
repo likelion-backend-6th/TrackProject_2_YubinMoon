db06a3a1ea1bf7c2a39c45b161088e3d562578e1
terry <munu220@gmail.com>
2023-09-06 13:27:15 +0000

    [feat] 모델 추가

--- follow_app/tweet/admin.py
+++ follow_app/tweet/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Follow, Image, Post
+
+admin.site.register(Follow)
+admin.site.register(Image)
+admin.site.register(Post)
--- follow_app/tweet/models.py
+++ follow_app/tweet/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    follow = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followed_by"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["follow"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user} follows {self.follow}"
+
+
+class Image(models.Model):
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.url}"
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["owner"]),
+        ]
+
+    def __str__(self):
+        return f"{self.owner} at {self.created_at}"
