dd092f90ab3f85d529b305d1bd5e7b6c0e828790
terry <munu220@gmail.com>
2023-09-06 17:09:58 +0000

    [fix] 필요 없는 코드 수정

--- follow_app/tweet/tests.py
+++ follow_app/tweet/tests.py
@@ -7,30 +7,24 @@ from .models import Follow, Image, Post
 
 
 # Create your tests here.
-class PostTest(APITestCase):
+class UserTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.basic_user = User.objects.create(username="basic", password="basic")
-        cls.follow_user = User.objects.create(username="follower", password="follower")
         cls.not_follow_user_list = []
         for i in range(5):
             user = User.objects.create(
                 username=f"un_follow_{i}", password=f"un_follow_{i}"
             )
-            Follow.objects.create(user=cls.follow_user, follow=user)
             cls.not_follow_user_list.append(user)
         cls.follow_user_list = []
         for i in range(5):
             user = User.objects.create(username=f"follow_{i}", password=f"follow_{i}")
             Follow.objects.create(user=cls.basic_user, follow=user)
-            Follow.objects.create(user=cls.follow_user, follow=user)
-            Follow.objects.create(user=user, follow=cls.follow_user)
             cls.follow_user_list.append(user)
 
         for i in range(5):
             Post.objects.create(owner=cls.basic_user, content=f"content_{i}")
-        for i in range(5):
-            Post.objects.create(owner=cls.follow_user, content=f"content_{i}")
 
     def test_get_all_user(self):
         self.client.force_login(self.basic_user)
@@ -46,5 +40,13 @@ class PostTest(APITestCase):
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.content)
         user_posts = Post.objects.filter(owner=self.basic_user)
-        print(data)
         self.assertEqual(len(data), len(user_posts))
+
+
+class PostTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_get_all_post(self):
+        pass
