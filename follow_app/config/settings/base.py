47886039a2455592b5374ce334f1fbda8dd758cb
terry <munu220@gmail.com>
2023-09-08 11:03:05 +0000

    [feat] 로그인 되지 않은 유저 차단

--- follow_app/config/settings/base.py
+++ follow_app/config/settings/base.py
@@ -56,13 +56,19 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    # YOUR SETTINGS
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Your Project API",
-    "DESCRIPTION": "Your project description",
+    "TITLE": "TrackProject Follow API",
+    "DESCRIPTION": "트렉 프로젝트 follow앱 API 문서입니다.",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
